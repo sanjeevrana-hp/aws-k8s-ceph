2b7fb063f9e01f962636ab90b38d214161f2c47b
Sanjeev Rana <45759930+sanjeevrana-hp@users.noreply.github.com>
2023-05-31 11:18:04 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "host_names" {
+  type    = list(any)
+  default = ["kube-worker-1", "kube-worker-2", "kube-worker-3"]
+}
+
+variable "instance_type" {
+  default = "t2.large"
+}
+
+variable "region" {
+  default = "ap-northeast-1"
+}
+
+variable "aws_shared_credentials_file" {
+  type    = string
+  default = "~/.aws/credentials"
+}
+
+variable "aws_profile" {
+  type    = string
+  default = "default"
+}
+
+variable "name" {
+}
